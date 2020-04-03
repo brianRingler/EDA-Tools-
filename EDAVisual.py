793803f50f8051d21de107a3242d68366f62cfb7
Brian Ringler <ringler.brian.8286@gmail.com>
2020-04-03 13:50:48 -0400

    Update EDAVisual.py

--- EDAVisual.py
+++ EDAVisual.py
@@ -1,26 +1,25 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
 class OptionsHist():
-    """The parent to histogram.py which allows users to quickly create standard
-    histograms during initial data analysis process.
-    """
+    """The parent class to pre-constructed Matplotlib histograms"""
 
     def __init__(self, dataframe, col_name, title, x_name, y_name):
-    """[summary]
-    
-    Arguments:
-        dataframe {[type]} -- [description]
-        col_name {[type]} -- [description]
-        title {[type]} -- [description]
-        x_name {[type]} -- [description]
-        y_name {[type]} -- [description]
-    """
+        """Predefined histogram configurations. 
+        
+        Arguments:
+            dataframe {pandas.DataFrame} -- DataFrame containing columns 
+            col_name {pandas.Series} -- str name of column from pandas dataframe
+            title {str} -- str name of chart title
+            x_name {str} -- str name of x-axis 
+            y_name {str} -- str name of y-axis
+            xticks {int} -- fontsize (14) setting of values on x-axis
+            yticks {int} -- fontsize (14) setting of values on y-axis
+        """
         self.dataframe = dataframe
         self.col_name = col_name
         self.x_name = plt.xlabel(x_name,fontsize=14)
         self.y_name = plt.ylabel(y_name,fontsize=14)
         self.title = plt.title(title,fontsize=14)
         self.xticks = plt.xticks(fontsize=14)
-        self.yticks = plt.yticks(fontsize=14)
+        self.yticks = plt.yticks(fontsize=14) 
          
